cfe92f8a31e63e73e9e2ec73a0d00cd259562697
rriver52 <rriver52>
2022-04-24 13:57:24 -0500

    new

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,6 +3,6 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen-nonav">
     <h2 class="text-6xl tracking-tighter font-extralight">About Page</h2>
-    <img src="../assets/bgabout.png" alt="Hello BG" />
+    <img src="../assets/about.png" alt="Hello BG" />
   </div>
 </template>
